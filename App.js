e0dc6b894712224d179da293a4de7ad135271212
Varun <varunshiri99@gmail.com>
2023-02-04 17:14:21 -0500

    Integrate camera

--- App.js
+++ App.js
@@ -2,15 +2,9 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Home from './components/Home/Home';
 
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
 
 function SettingsScreen() {
   return (
@@ -27,8 +21,20 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Home" options={{
+          tabBarLabel: 'Home', tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+          component={Home} />
+        <Tab.Screen
+          options={{
+            tabBarLabel: 'Settings', tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="cogs" color={color} size={26} />
+            ),
+          }}
+          name="Settings"
+          component={SettingsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
