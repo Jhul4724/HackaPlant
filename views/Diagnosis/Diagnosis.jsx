41b26d9d5a10773b291e82b1760d8121e2bcfb93
Thomas <thomas-frederic.beaugendre@polymtl.ca>
2023-02-04 23:36:32 -0500

    Plant card photo

--- views/Diagnosis/Diagnosis.jsx
+++ views/Diagnosis/Diagnosis.jsx
@@ -1,60 +1,99 @@
-import axios from 'axios';
-import * as React from 'react';
-import { View, Image } from 'react-native';
-import { ActivityIndicator, Button, Dialog, Portal, Provider, Text } from 'react-native-paper';
-
+import axios from "axios";
+import * as React from "react";
+import { View, Image } from "react-native";
+import {
+  ActivityIndicator,
+  Button,
+  Dialog,
+  Portal,
+  Provider,
+  Text,
+  Card,
+} from "react-native-paper";
 
 export default function Diagnosis({ route, navigation }) {
-    const [data, setData] = React.useState({});
-    const [visible, setVisible] = React.useState(false);
-    const [loading, setLoading] = React.useState(false);
+  const [data, setData] = React.useState({});
+  const [visible, setVisible] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  const showDialog = () => setVisible(true);
+  const hideDialog = () => {
+    setVisible(false);
+    navigation.navigate("Home");
+  };
+
+  const getDisease = async () => {
+    try {
+        const response = await axios.post('https://api.plant.id/v2/health_assessment', {
+          "api_key": "juBFAqnP7TIFsOFo8qdHiah5fmhXXOAHBhS9Di8vNsdKrvA7WY",
+          "modifiers": ["crops_fast"],
+          "disease_details": ["common_names", "description", "local_name", "treatment"],
+          "images": [route.params.base64],
+      });
+      return response.data;
+    }
+    catch (error) {
+      console.log(error);
+    }
+};
 
 
-    const showDialog = () => setVisible(true);
-    const hideDialog = () => {
-        setVisible(false);
-        navigation.navigate('Home')
-    };
+  React.useEffect(() => {
+    (async () => {
+      setLoading(true);
+      const disease = await getDisease();
+      setLoading(false);
+      if (!disease.is_plant)
+          showDialog();
+      else {
+          console.log(JSON.stringify(disease));
+          setData(disease);
+      }
+    })();
+  }, [route.params]);
 
-    React.useEffect(() => {
-        (async () => {
-            setLoading(true);
-            const response = await axios.post('https://api.plant.id/v2/health_assessment', {
-                "api_key": "juBFAqnP7TIFsOFo8qdHiah5fmhXXOAHBhS9Di8vNsdKrvA7WY",
-                "modifiers": ["crops_fast"],
-                "disease_details": ["common_names", "description", "local_name", "treatment"],
-                "images": [route.params.base64]
-            });
-            setLoading(false);
-            if (!response.data.is_plant)
-                showDialog();
-            else {
-                console.log(JSON.stringify(response.data));
-                setData(response.data);
-            }
-        })()
-
-    }, [route.params])
+  if (loading)
+  return(
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+     <ActivityIndicator />
+    </View>
+  )
+  else
     return (
-        <Provider>
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                {loading ? <ActivityIndicator /> :
-                    <>
-                        {data.health_assessment?.diseases && data.health_assessment.diseases.map(disease => <Text>{disease.name}</Text>)}
-                        {route.params && <Image source={{ uri: route.params.uri }} style={{ width: 200, height: 200 }} />}
-                        <Portal>
-                            <Dialog visible={visible} onDismiss={hideDialog}>
-                                <Dialog.Title>Alert</Dialog.Title>
-                                <Dialog.Content>
-                                    <Text variant="bodyMedium">Oops! Looks like your image doesn't have a plant. Go look for some plants!</Text>
-                                </Dialog.Content>
-                                <Dialog.Actions>
-                                    <Button onPress={hideDialog}>OK</Button>
-                                </Dialog.Actions>
-                            </Dialog>
-                        </Portal>
-                    </>}
-            </View>
-        </Provider>
-    )
+      <Provider>
+        <View>
+          {data.health_assessment?.diseases &&
+            data.health_assessment.diseases.map((disease) => (
+              <Text>{disease.name}</Text>
+            ))}
+            {console.log(data.health_assessment)}
+          <Card>
+            <Card.Cover source={{ uri: route.params.uri }} />
+            <Card.Content>
+              <Text variant="titleLarge"> Plant name </Text>
+            </Card.Content>
+          </Card>
+          {route.params && (
+            <Image
+              source={{ uri: route.params.uri }}
+              style={{ width: 200, height: 200 }}
+            />
+          )}
+          <Portal>
+            <Dialog visible={visible} onDismiss={hideDialog}>
+              <Dialog.Title>Alert</Dialog.Title>
+              <Dialog.Content>
+                <Text variant="bodyMedium">
+                  Oops! Looks like your image doesn't have a plant. Go look for
+                  some plants!
+                </Text>
+              </Dialog.Content>
+              <Dialog.Actions>
+                <Button onPress={hideDialog}>OK</Button>
+              </Dialog.Actions>
+            </Dialog>
+          </Portal>
+        </View>
+      </Provider>
+    );
 }
