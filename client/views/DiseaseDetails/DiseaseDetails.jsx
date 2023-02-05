50870bf9b5587a21c82775af269be20825e53a5a
Varun <varunshiri99@gmail.com>
2023-02-05 05:33:09 -0500

    Merge to remote

--- client/views/DiseaseDetails/DiseaseDetails.jsx
+++ client/views/DiseaseDetails/DiseaseDetails.jsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import { Button, Chip, Text, Card } from 'react-native-paper';
 import Data from "./response.json";
+import { List, MD3Colors } from 'react-native-paper';
+
 
 export default function DiseaseDetails() {
     const disease = Data.health_assessment.diseases[0];
@@ -11,14 +13,28 @@ export default function DiseaseDetails() {
                 <Text variant='headlineMedium' style={{ fontWeight: "bold" }}>{disease.name}</Text>
                 <Chip icon="information">Certainty: {Math.round(disease.probability * 100)}</Chip>
             </View>
-            <View style={{ padding: 10 }}>
+            <View style={{ padding: 10, flex: 1 }}>
                 <Card>
-                    <Card.Title style={{ fontWeight: "bold" }} title="Description" />
+                    <Card.Title titleStyle={{ fontWeight: "bold" }} title="Description" />
                     <Card.Content>
-                        <Text variant="displatMedium">{Data.health_assessment.diseases[0].disease_details.description}</Text>
+                        <Text variant="displatMedium">{disease.disease_details.description}</Text>
                     </Card.Content>
                 </Card>
-                <Text variant='titleMedium' style={{ fontWeight: "bold" }}>{Data.health_assessment.is_healthy ? "Possibile diseases" : "Diseases detected"}</Text>
+                <Text variant='titleLarge' style={{ fontWeight: "bold", marginTop: 15 }}>Treatments</Text>
+                <ScrollView>
+                    <Card style={{ margin: 5, padding: 5 }}>
+                        <Card.Title titleStyle={{ fontWeight: "bold" }} title="Biological" />
+                        <Card.Content>{disease.disease_details.treatment.biological.map((desc, i) => <List.Item style={{ paddingHorizontal: 5 }} descriptionNumberOfLines={5} description={desc} left={() => <List.Icon icon={`numeric-${i > 9 ? '9-plus' : i + 1}-circle`} />} />)}</Card.Content>
+                    </Card>
+                    <Card style={{ margin: 5, padding: 5 }}>
+                        <Card.Title titleStyle={{ fontWeight: "bold" }} title="Chemical" />
+                        <Card.Content>{disease.disease_details.treatment.chemical.map((desc, i) => <List.Item style={{ paddingHorizontal: 5 }} descriptionNumberOfLines={5} description={desc} left={() => <List.Icon icon={`numeric-${i > 9 ? '9-plus' : i + 1}-circle`} />} />)}</Card.Content>
+                    </Card>
+                    <Card style={{ margin: 5, padding: 5 }}>
+                        <Card.Title titleStyle={{ fontWeight: "bold" }} title="Prevention" />
+                        <Card.Content>{disease.disease_details.treatment.prevention.map((desc, i) => <List.Item style={{ paddingHorizontal: 5 }} descriptionNumberOfLines={5} description={desc} left={() => <List.Icon icon={`numeric-${i > 9 ? '9-plus' : i + 1}-circle`} />} />)}</Card.Content>
+                    </Card>
+                </ScrollView>
 
             </View>
         </>
