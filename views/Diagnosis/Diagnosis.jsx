baa6f3ac32ff74f1593ed4529cbca9bb8bdb57f3
Thomas <thomas-frederic.beaugendre@polymtl.ca>
2023-02-04 22:34:39 -0500

    Disease name

--- views/Diagnosis/Diagnosis.jsx
+++ views/Diagnosis/Diagnosis.jsx
@@ -40,7 +40,7 @@ export default function Diagnosis({ route, navigation }) {
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 {loading ? <ActivityIndicator /> :
                     <>
-                        {data.diseases && data.diseases.map(disease => <Text>{disease.name}</Text>)}
+                        {data.health_assessment?.diseases && data.health_assessment.diseases.map(disease => <Text>{disease.name}</Text>)}
                         {route.params && <Image source={{ uri: route.params.uri }} style={{ width: 200, height: 200 }} />}
                         <Portal>
                             <Dialog visible={visible} onDismiss={hideDialog}>
