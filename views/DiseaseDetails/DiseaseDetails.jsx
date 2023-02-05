11645ee43bfe9fbeb9e69dd62445b6f7c671b433
Varun <varunshiri99@gmail.com>
2023-02-04 23:22:12 -0500

    Separate client

---
+++ views/DiseaseDetails/DiseaseDetails.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Text } from 'react-native-paper';
+import Data from "./response.json";
+
+export default function DiseaseDetails() {
+    const disease = Data.health_assessment.diseases[0];
+
+    return (
+        <>
+
+        </>
+    )
+}
