96c538e50873457161a59a9a0ba77e4c24234637
Varun <varunshiri99@gmail.com>
2023-02-05 02:22:49 -0500

    Create server and image upload

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import multer from 'multer';
+import { database, storage } from './firebase.js';
+const app = express();
+
+const upload = multer({
+    storage: multer.memoryStorage()
+})
+app.use(express.static('uploads'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.post('/upload', upload.single('image'), (req, res) => {
+    storage.bucket().file(req.file.originalname).save(req.file.buffer);
+    database.ref('uploads').push(req.file.originalname);
+    res.send({ file: req.file.originalname });
+})
+
+app.listen(5000, () => console.log("Server listening on port 5000"));
